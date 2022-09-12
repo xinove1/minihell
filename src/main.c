f10d5d87acd73bbdaa7e11bd25585e74edbbe48e
xinove1 <xinovebig@gmail.com>
2022-09-12 16:10:17 -0300

    Basic structure

--- src/main.c
+++ src/main.c
@@ -1,17 +1,61 @@
 #include "minihell.h"
 
-int main(int argc, char *argv[])
-{
-	//TODO deal with user passing args to minishell
-	(void) argc;
-	(void) argv;
+static void	shell_loop(t_data *data);
+void	parse_cmd(t_data *data, char *line);
 
-	char *line;
+int	main(int argc, char *argv[], char *envp[])
+{
+	t_data	data;
+
+	(void) argv;
+	(void) envp;
+	if (argc > 1)
+	{
+		ft_putstr_fd("Error\nMinishell does not accept any arguments. Please check the README.\n", 2);
+		exit(1);
+	}
+
+	// TODO init data
+	// TODO parse envp, hash table maybe?
+	// TODO setup signals
+
+	shell_loop(&data);
+	return (0);
+}
+
+static void	shell_loop(t_data *data)
+{
+	char	*line;
+
 	while (1)
 	{
+		pwd(1, NULL);
+		//NOTE prompt
 		line = readline("minishell >");
 		add_history(line);
+		parse_cmd(data, line);
+		//Exec tree
 		free(line);
 	}
-	return (0);
+}
+
+//should return first node or null
+void	parse_cmd(t_data *data, char *line)
+{
+	(void) data;
+
+	// NOTE Also check there is only cd/exit on the line
+	// cd/exit followed by newline should work too
+	if (!ft_strncmp("cd ", line, 3))
+	{
+		cd(2, (char *[2]){"cd", line + 3});
+		return ;
+	}
+	else if (!ft_strncmp("exit ", line, 5))
+	{
+		//CLEAN memory
+		exit(EXIT_SUCCESS);
+	}
+	// parse line
+	// return node
 }
--- src/minihell.h
+++ src/minihell.h
@@ -3,15 +3,64 @@
 # include "../libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
+# include <fcntl.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <errno.h>
 
+// LIMITS
 # define PATH_MAX 1024
 
+typedef enum e_cmd_types {EXEC, PIPE, REDIR}	t_cmd_types;
+
+// Structs
+typedef struct s_cmd
+{
+	t_cmd_types	type;
+}	t_cmd;
+
+typedef struct s_execcmd
+{
+	t_cmd_types	type;
+	int			builtin;
+	char		*exec;
+	char		**args;
+}	t_execcmd;
+
+typedef struct s_pipecmd
+{
+	t_cmd_types	type;
+	t_cmd		*left;
+	t_cmd		*right;
+}	t_pipecmd;
+
+typedef struct s_redircmd
+{
+	t_cmd_types	type;
+	t_cmd		*cmd;
+	char		*file;
+	int			mode;
+	int			fd;
+}	t_redircmd;
+
+typedef struct s_doccmd
+{
+	t_cmd_types	type;
+	t_cmd		*cmd;
+	char		*eof;
+}	t_doccmd;
+
+typedef struct s_data
+{
+	// envp in whatever format
+	// pointer to first node for freeing later
+	// current node?
+	// Exit code from last node?
+}	t_data;
+
 //Built-ins
-int echo(int argc, char *argv[]);
-int pwd(int argc, char *argv[]);
-int cd(int argc, char *argv[]);
+int	echo(int argc, char *argv[]);
+int	pwd(int argc, char *argv[]);
+int	cd(int argc, char *argv[]);
 
 #endif // MINIHELL_H_
